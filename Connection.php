554e996bd4c2feed4b6d79df024ff10c7b1446fe
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-26 15:31:46 -0600

    Cambios a la estructura de la api para la nueva BD

--- Connection.php
+++ Connection.php
@@ -1,21 +1,25 @@
 <?php
 
-class Connection extends PDO
+header('Content-Type: application/json;charset=utf-8');
+
+
+function connection()
 {
-    private string $host = '156.67.73.201';
-    private string $dbName = 'u551768457_world';
-    private string $user = 'u551768457_admin';
-    private string $password = 'Admin12345.';
-
-    public function __construct()
-    {
-        try {
-            parent::__construct("mysql:host=$this->host;dbname=$this->dbName;charset=utf8", $this->user, $this->password);
-            parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-            exit;
-        }
+    $port = 1433;
+    $serverName = "tcp:sd-oal-02.database.windows.net,$port";
+    $database = "BikeStores";
+    $userName = "Student";
+    $password = "Pa55w.rd";
+    try {
+        $conn = new PDO("sqlsrv:server = $serverName,$port; Database = $database", $userName, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        echo json_encode(array(
+            'ok' => false,
+            'message' => 'Connection to database failed'
+        ), JSON_UNESCAPED_UNICODE);
+        die();
     }
 }
--- index.php
+++ index.php
@@ -9,36 +9,23 @@ header('Content-Type: application/json;charset=utf-8');
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $conn = new Connection();
+        $conn = connection();
         $ip = getIpAddress();
-        // $countryCode = getCountryCode('187.191.15.151'); // dev
-        $countryCode = getCountryCode($ip); // production
+        $countryCode = getCountryCode('187.191.15.151'); // dev
+        // $countryCode = getCountryCode($ip); // production
         $citys = array();
         $countryName = '';
 
-        $sql = "select ci.District, co.Name from city as ci
-                inner join country as co on co.Code = ci.CountryCode
-                where co.Code2 like '$countryCode' group by ci.District order by ci.District;";
+        $sql = "SELECT * FROM [production].[products]";
         $query = $conn->prepare($sql);
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        $countryName = $result[0]['Name'];
 
-        foreach ($result as $city) {
-            if ($countryCode == 'SG') {
-                array_push($citys, $city['District']);
-            } else {
-                array_push($citys, utf8_decode($city['District']));
-            }
-        }
+
 
         $apiResponse = array(
             'ok' => true,
-            'result' => array(
-                'country_code' => $countryCode,
-                'country_name' => $countryName,
-                'citys' => $citys,
-            ),
+            'result' => $result,
         );
         echo json_encode($apiResponse, JSON_UNESCAPED_UNICODE);
         http_response_code(200);
@@ -48,9 +35,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         header('HTTP/1.1 405 Method Not Allowed');
         echo json_encode(array(
             'ok' => false,
-            'result' => array(
-                'message' => 'Method not allowed'
-            )
+            'result' => 'Method not allowed'
         ), JSON_UNESCAPED_UNICODE);
         http_response_code(405);
         break;
